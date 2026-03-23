0e4ed3e0ab334408423ce30810dabbd2aa84e616
Zachary Edwards <z.edwards@ljmu.ac.uk>
2026-03-23 10:21:00 +0000

    spook

--- script.py
+++ script.py
@@ -13,15 +13,14 @@ with open('cardiovascular_risk_dataset.csv', 'r') as f:
         AlcoholUnitsDrunk.append(data)
         dat = float(row[13])
         DietQuality.append(dat)
-    AlcoholUnitsDrunk.sort()
-    DietQuality.sort()
+    #AlcoholUnitsDrunk.sort()
+    #DietQuality.sort()
     print (len(AlcoholUnitsDrunk), len(DietQuality))
     fig, ax = plt.subplots()
-    #ax.plot(AlcoholUnitsDrunk, DietQuality,'bo')
     plt.plot(AlcoholUnitsDrunk, DietQuality, 'go')
     plt.scatter(AlcoholUnitsDrunk, DietQuality, plotnonfinite=True, alpha=0.1)
     plt.xlabel("Alcohol Units drunk (au)")
     plt.ylabel("Diet Quality (1-10)")
     plt.show()
-
+    #produces a curve suggesting as diet quality increases, alcohol units increase at an increasing rate
 
