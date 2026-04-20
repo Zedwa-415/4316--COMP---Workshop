51b796395f00e8064dc11a2462d089f92c0e676a
Zachary Edwards <z.edwards@ljmu.ac.uk>
2026-04-20 10:07:28 +0100

    commit test

--- script.py
+++ script.py
@@ -4,6 +4,5 @@ import matplotlib.pyplot as pl
 import pandas as pd
 
 CardioData = pd.read_csv('cardiovascular_risk_dataset.csv')
-print ("hello")
 CardioData.plot(kind= 'scatter',x = "Alcohol Units drunk", y = "data quality")
 CardioData.show()
