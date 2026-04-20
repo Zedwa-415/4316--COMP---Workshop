da34dc7fcc2bd6d13e4fce1d8c1ff7d72525ac05
Zachary Edwards <z.edwards@ljmu.ac.uk>
2026-04-20 10:14:42 +0100

    test run

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
 #what is the relationship between diet quality and alcohol units drunk?
 import csv
+import sys
 import matplotlib.pyplot as pl
 import pandas as pd
+print ("dada")
 
 CardioData = pd.read_csv('cardiovascular_risk_dataset.csv')
 CardioData.plot(kind= 'scatter',x = "Alcohol Units drunk", y = "data quality")
