ab3b6134b07cd8b80f616cba8e0e8205178838b5
Zachary Edwards <CSMZEDWA@ljmu.ac.uk>
2026-04-20 11:54:41 +0100

    it finally workded

--- script.py
+++ script.py
@@ -6,5 +6,5 @@ import pandas as pd
 print ("dada")
 
 CardioData = pd.read_csv('cardiovascular_risk_dataset.csv')
-CardioData.plot(kind= 'scatter',x = "Alcohol Units drunk", y = "data quality")
-CardioData.show()
+CardioData.plot(kind = 'scatter', x = "diet_quality_score", y = "alcohol_units_per_week")
+pl.show()
